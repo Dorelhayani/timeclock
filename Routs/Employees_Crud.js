28cea87839a67726c9edfb674d7310f311c86ae6
dorel <dorelhayani@gmail.com>
2023-09-21 23:38:30 +0300

    frontend adding
    1.adding a main page named "EMP"
    (stand for employees main page).
    
    2.adding top and bottom pages.
    3. adding a css file
    
    fixing few bugs relating for displaying the EMP page. (changed the directory name from EmployeeView to view)

---
+++ Routs/Employees_Crud.js
@@ -0,0 +1,55 @@
+// Employees CRUD - BackEnd
+// ---------------------------------------------------------------------------------------------------------------------
+const express = require('express');
+const router = express.Router()
+module.exports = router;
+
+// ---------------------------------------------------------------------------------------------------------------------
+// rout to Main Page - " EMP "
+router.get("/",(req, res) => {
+
+    res.render("EMP", {pageTitle:"Employees"});
+
+});
+// ---------------------------------------------------------------------------------------------------------------------
+
+// Create Addpoint
+router.post("/Add",(req, res) => {
+    let name=req.body.name;
+    let q=`INSERT INTO \`employees\` (\`name\`) VALUES ('${name}')` ;
+    db_pool.query(q, function(err){
+        if(err){  res.status(500).json({message: err})  }
+        else{  res.status(200).json({message: "OK"});  }    });    });
+// ---------------------------------------------------------------------------------------------------------------------
+
+// Read Addpoint
+router.get("/List",(req, res) => {
+    let q="SELECT * FROM `employees` ";
+    db_pool.query(q, function(err, rows){
+        if(err)  {  res.status(500).json({message: err})  }
+        else {  res.status(200).json(rows );  }    });    });
+// ---------------------------------------------------------------------------------------------------------------------
+
+// Update Addpoint
+router.patch("/Update",(req, res) => {
+    let id=req.params.id;
+
+    let q=`UPDATE \`employees\`  SET \`name\`='${name}' WHERE id=${id} `;
+
+    db_pool.query(q, function(err){
+        if(err){ res.status(500).json({message: err}) }
+        else{  res.status(200).json({message: "OK"});  }    });    });
+// ---------------------------------------------------------------------------------------------------------------------
+
+// Delete Addpoint
+router.delete("/Delete",(req, res) => {
+    let id=req.params.id;
+
+    let q=`DELETE FROM \`employees\` WHERE id='${id}' `;
+
+    db_pool.query(q, function(err){
+
+        if(err){  res.status(500).json({message: err})  }
+
+        else {  res.status(200).json({message: "OK"});  }    });    });
+// ---------------------------------------------------------------------------------------------------------------------
--- index.js
+++ index.js
@@ -1,34 +1,31 @@
-// צד אחורי
+// Index - BackEnd
 
+// ---------------------------------------------------------------------------------------------------------------------
 const express = require('express');
 const port = 2233;
 const app = express();
 app.use(express.json());
-
+// ---------------------------------------------------------------------------------------------------------------------
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: false}));
-
+// ---------------------------------------------------------------------------------------------------------------------
 app.set("view engine", "ejs");
-
+// ---------------------------------------------------------------------------------------------------------------------
 const path = require('path');
 app.use(express.static(path.join(__dirname, "css")));
 app.use(express.static(path.join(__dirname, "js")));
-
+// ---------------------------------------------------------------------------------------------------------------------
 let db_M = require('./database');
 global.db_pool = db_M.pool;
-
+// ---------------------------------------------------------------------------------------------------------------------
 // BE Employees Table
-const Employees_rtr=require('./Routs/Employee_Crud')
+const Employees_rtr=require('./Routs/Employees_Crud');
 app.use('/employees', Employees_rtr);
-
+// ---------------------------------------------------------------------------------------------------------------------
 // BE TimeReport Table
-const Time_rtr=require('./Routs/Timereport _Crud')
+const Time_rtr=require('./Routs/Timereport _Crud');
 app.use('/timereport', Time_rtr);
-
-// Category FE
-// const fe_rtr = require('./Routs/FE_R');
-// app.use('/FE', fe_rtr);
-
-
-
+// ---------------------------------------------------------------------------------------------------------------------
+// ---------------------------------------------------------------------------------------------------------------------
 app.listen(port, () => { console.log(`Now listening on port http://localhost:${port}`); });
+// ---------------------------------------------------------------------------------------------------------------------
