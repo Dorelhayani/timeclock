ebdc3c3595824fd4713b5a49096562349431eac9
dorel <dorelhayani@gmail.com>
2023-09-28 19:02:48 +0300

    timereport CRUD is working flawlessly

--- Routs/Timereport _Crud.js
+++ Routs/Timereport _Crud.js
@@ -9,7 +9,7 @@ router.get("/",(req, res) => {
 // Create Addpoint
 router.post("/Add",(req, res) => {
     let id = req.body.employeeID;
-    let Q = `INSERT INTO timereport(employeeID,Clockin,Date) VALUES(${id},CURRENT_TIMESTAMP(),Date)`
+    let Q = `INSERT INTO timereport(employeeID,Clockin,Date) VALUES(${id},CURRENT_TIMESTAMP(), DATE_FORMAT(CURRENT_DATE(),'%Y-%m-%d' ))`;
     db_pool.query(Q, function(err){
         if (err){
             res.status(500).json({message: err}); }
@@ -18,24 +18,13 @@ router.post("/Add",(req, res) => {
 // ---------------------------------------------------------------------------------------------------------------------
 // Read Addpoint
 router.get("/List",(req, res) => {
-    // let Q="SELECT * FROM `timereport` ";
     let Q=
     `
-        SELECT e.employeeID, e.Name, t.Clockin, t.Clockout
+        SELECT e.employeeID, e.Name, t.Clockin, t.Clockout, t.Date
         FROM employees e
         LEFT JOIN \`timereport\` t ON e.employeeID = t.employeeID
-        WHERE t.Clockin IS NOT NULL AND t.Clockout IS NOT NULL
+        WHERE t.Clockin IS NOT NULL AND t.Clockout IS NOT NULL AND t.Date IS NOT NULL
     `
-
-    // let Q=
-    //     `
-    //     SELECT e.employeeID, e.Name, t.Clockin, t.Clockout
-    //     FROM employees e
-    //     LEFT JOIN \`timereport\` t ON e.employeeID = t.employeeID
-    //
-    // `
-
-
     db_pool.query(Q, function(err, rows){
         if(err)  {  res.status(500).json({message: err})  }
         else {  res.status(200).json(rows );  }    });    });
@@ -43,7 +32,7 @@ router.get("/List",(req, res) => {
 // Update Clockin Addpoint
 router.patch("/Clockin",(req, res) => {
     let id=req.body.employeeID;
-    let Q = `UPDATE \`timereport\` SET \`Clockin\` = CURRENT_TIMESTAMP() WHERE employeeID = ${id}`;
+    let Q = `UPDATE timereport SET Clockin = CURRENT_TIMESTAMP() WHERE employeeID = ${id}`;
     db_pool.query(Q, function(err){
         if(err){ res.status(500).json({message: err}) }
         else{  res.status(200).json({message: "OK"});  }    });    });
@@ -51,7 +40,7 @@ router.patch("/Clockin",(req, res) => {
 // Update Clockout Addpoint
 router.patch("/Clockout",(req, res) => {
     let id=req.body.employeeID ;
-    let Q =`UPDATE \`timereport\`  SET \`Clockout\`= CURRENT_TIMESTAMP() WHERE \`employeeID\`=${id} `;
+    let Q =`UPDATE timereport SET Clockout= CURRENT_TIMESTAMP() WHERE employeeID=${id} `;
     db_pool.query(Q, function(err){
         if(err){ res.status(500).json({message: err}) }
         else{  res.status(200).json({message: "OK"});  }    });    });
